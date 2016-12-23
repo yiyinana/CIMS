49fb7a1e6aea504865a127535d9fc90dbc903dc6
yiyinana <zhang906337636@163.com>
2016-12-23 09:17:06 +0800

    张姣娜：添加后台导出进度

--- src/com/base/constants/SessionKeyConstants.java
+++ src/com/base/constants/SessionKeyConstants.java
@@ -26,4 +26,6 @@ public class SessionKeyConstants {
 	 */
 	public static String BEGIN_YEAR = "begin_year";
 	public static String END_YEAR = "end_year";
+	
+	public static String selectProjectSummaryList="";
 }
